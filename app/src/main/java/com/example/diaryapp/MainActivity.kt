161e8f98907269d6a2c4fe48cbf0f6f2b364eaef
Ahmet Bozyurt <ahmet.bozyurt@leanscale.com>
2024-01-15 21:58:55 +0300

    Add theme and splash screen

--- app/src/main/java/com/example/diaryapp/MainActivity.kt
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryAppTheme {
                 // A surface container using the 'background' color from the theme
